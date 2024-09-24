161f3032e364767af35da3e41b89f83d55949094
aryya <aryyawijaya@gmail.com>
2024-09-24 14:59:33 +0000

    feat: init app

---
+++ db/db.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"strconv"
+
+	_ "github.com/lib/pq" // don't forget to add it. It doesn't be added automatically
+)
+
+var Db *sql.DB //created outside to make it global.
+
+// make sure your function start with uppercase to call outside of the directory.
+func ConnectDatabase() {
+
+	host := os.Getenv("POSTGRES_HOST")
+	port, _ := strconv.Atoi(os.Getenv("POSTGRES_PORT")) // don't forget to convert int since port is int type.
+	user := os.Getenv("POSTGRES_USER")
+	dbname := os.Getenv("POSTGRES_DB")
+	pass := os.Getenv("POSTGRES_PASSWORD")
+
+	// fmt.Printf("host=%s\nport=%d\nuser=%s\ndbname=%s\npass=%s", host, port, user, dbname, pass)
+
+	// set up postgres sql to open it.
+	psqlSetup := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
+		host, port, user, dbname, pass)
+	db, errSql := sql.Open("postgres", psqlSetup)
+	if errSql != nil {
+		fmt.Println("There is an error while connecting to the database ", errSql)
+		panic(errSql)
+	} else {
+		Db = db
+		fmt.Println("Successfully connected to database!")
+	}
+}
